5840aa281f505b983c97363ab33092d103967b81
Aadarsh <aadarshshekhar22@gmail.com>
2024-07-12 17:25:28 +0530

    first frontend checkpoint

--- components/ui/shared/Footer.tsx
+++ components/ui/shared/Footer.tsx
@@ -1,6 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex flex-center wrapper flex-col gap-2 p-2 text-center">
+        <Link href = '/'>
+        <Image 
+        src="/assets/logo.svg"
+        alt="logo"
+        height={34}
+        width={34}
+        />
+        </Link>
+
+        <p>2024 College Compass. All Rights Reserved </p>
+      </div>
+    </footer>
   )
 }
 
