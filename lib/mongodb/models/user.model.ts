a3920fc60f8e7add17616b9f14c62d820cfd347b
Aadarsh <aadarshshekhar22@gmail.com>
2024-07-13 11:51:56 +0530

    last commit before deploying

--- lib/mongodb/models/user.model.ts
+++ lib/mongodb/models/user.model.ts
@@ -4,7 +4,7 @@ import { model, models, Schema } from "mongoose";
 const userSchema = new Schema({
     clerkID: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
-    userName: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     photo: {type: String, required: true}
